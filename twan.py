44d3bb47623f67edf465cdbea9491ffe3f38e8f1
Bram-en-twan <52704568+Bram-en-twan@users.noreply.github.com>
2019-09-24 16:39:37 +0200

    Add files via upload

--- twan.py
+++ twan.py
@@ -18,14 +18,18 @@ Label(newscreen,image=img2,bd=0).place(x=40,y=40)
 Label(newscreen,image=img3,text="Te besteden: \n\n $500",font=('arial',25), compound=tkinter.CENTER,bd=0,bg="black").place(x=470,y=40)
 Label(newscreen,image=img3,bd=0).place(x=470,y=280)
 Label(newscreen,image=img3,bd=0).place(x=470,y=520)
-Entry(newscreen,font=('arial',15),textvariable=money).place(x=490,y=320)
+Entry(newscreen,bd=0, font=('arial',15),textvariable=money).place(x=585,y=394, width=197,height=65)
+Label (newscreen,text="invoer:",font=('arial',15,)).place(x=585,y=308, width=197,height=65)
 def add():
     moneycount = money.get()
     if "+" in moneycount:
         print(f"plus{moneycount}")
     if "-" in moneycount:
         print(f"min{moneycount}")
-Button(newscreen, font=('arial',15),text=(">"),command=add).place(x=490,y=350)
+Button(newscreen,bd=0,  font=('arial',15),text=("<-"),command=add).place(x=809,y=308, width=65,height=150)
+Button(newscreen,bd=0, bg="limegreen", font=('arial',15),text=("+"),command=add).place(x=500,y=308, width=65,height=65)
+Button(newscreen,bd=0, bg="red", font=('arial',15),text=("-"),command=add).place(x=500,y=396, width=65,height=65)
+
 #Label(newscreen,bg='black',width=20,height=4,text='$500',fg="white",font=('arial',20,'bold')).place(x=600,y=140)
 #f3.create_text (0, 0, text ="$5464567", font = ("arial", 20,'bold'))
 
