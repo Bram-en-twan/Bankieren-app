547bba9ff9eb85e8d0f9e613bbc2c33a6a5557eb
Bram-en-twan <52704568+Bram-en-twan@users.noreply.github.com>
2019-09-24 19:12:17 +0200

    Add files via upload

--- inlogscherm bankieren-app.py
+++ inlogscherm bankieren-app.py
@@ -13,21 +13,22 @@ font1=("Arial",20)
 
 value1=StringVar()
 
-Label(newscreen,image=img1,bd=0).place(x=650, y=0)
+
+Label(newscreen,image=img1,bd=0).place(x=450, y=0)
 
 box=ttk.Combobox(newscreen,font=font1,state='readonly')
 box['values']=("gebruiker1","gebruiker2","gebruiker3")
 box.current(0)
-box.place(x=70,y=200)
+box.place(x=65,y=275, width=300,height=75)
 
-Entry(newscreen,show='*',textvariable=value1,bd=2,font=font1,relief='groove').place(x=70,y=300)
+Entry(newscreen,show='*',textvariable=value1,bd=2,font=font1,relief='groove').place(x=65,y=400, width=300,height=75)
 
 def test():
-    if value1.get() == "gebruiker1":
+    if value1.get() == "1":
         Frame(newscreen,width=8000,height=1000,bg="white").place(x=0,y=0)
                
     else:
-        Label(newscreen,width=20,height=2,font=("Arial",20),bg="black",text="wachtwoord onjuist",bd=0,fg="white").place(x=80,y=400)
+        Button(newscreen,width=20,height=2,font=("Arial",20),bg="black",text="wachtwoord onjuist",bd=0,fg="white",command=test).place(x=20,y=400)
     value1.set("")
 
-Button(newscreen,width=20,height=1,font=("Arial",20),bg="black",text="inloggen",bd=0, fg="white",command=test).place(x=70,y=400)
+Button(newscreen,width=20,height=1,font=("Arial",25),bg="black",text="inloggen",bd=0, fg="white",command=test).place(x=20,y=500)
